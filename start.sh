aa1d0018fcc81185f1c7a5ab47ad151c1bfb9fa6
Leo <cli4168@hillstonenet.com>
2024-07-02 15:04:31 +0800

    generate range with configurations and templates

--- start.sh
+++ start.sh
@@ -2,6 +2,15 @@
 
 vagrant destroy -f
 rm -rf data; mkdir data
+
+./script_generator attacker_provision.templ range.yml > data/attacker_provision
+./script_generator victim_provision.templ range.yml > data/victim_provision
+./script_generator handler.rc.templ range.yml > data/handler.rc
+echo Provision scripts generated!
+
 vagrant up
 sleep 10
-vagrant ssh attacker -c 'tmux send -t collector:0 "ls -l" ENTER'
+vagrant ssh attacker -c 'tmux send -t collector:0 "shell" ENTER'
+sleep 2
+vagrant ssh attacker -c 'tmux send -t collector:0 "bash /vagrant/c2commands" ENTER'
+echo C2 commands executed!
--- victim_provision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp /vagrant/data/shell.elf .
-chmod 755 shell.elf
-nohup ./shell.elf 1> /tmp/beacon.out 2> /tmp/beacon.err &
-echo "beacon started!"
